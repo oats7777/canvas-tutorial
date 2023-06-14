231d3d748105653540a186bbda3249c699dadab7
kyu <kyu@spoonradio.co>
2023-06-14 17:48:49 +0900

    투명도가 다른 사각형 두개 그리기

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ function draw() {
     'tutorial'
   ) as HTMLCanvasElement | null;
   if (canvas?.getContext) {
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d')!;
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
   }
 }
 
