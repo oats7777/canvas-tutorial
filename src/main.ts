ca3d97be9de92f68d90cea12379b1f8dbcea76b1
kyu <kyu@spoonradio.co>
2023-06-16 18:08:53 +0900

    moveTo 학습

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import './style.css';
 
+const getCtx = () => {
+  const canvas = document.getElementById(
+    'tutorial'
+  ) as HTMLCanvasElement | null;
+  if (canvas?.getContext) {
+    return canvas.getContext('2d')!;
+  }
+  return null;
+};
+
 function drawSquare() {
   // fillRect(x, y, width, height) - 색칠된 직사각형을 그립니다.
   // strokeRect(x, y, width, height) - 직사각형 윤곽선을 그립니다.
   // clearRect(x, y, width, height) - 특정 부분을 지우는 직사각형을 그립니다. 이 지워진 부분은 완전히 투명해집니다.
 
-  const canvas = document.getElementById(
-    'tutorial'
-  ) as HTMLCanvasElement | null;
-  if (canvas?.getContext) {
-    const ctx = canvas.getContext('2d')!;
-
+  const ctx = getCtx();
+  if (ctx) {
     ctx.fillRect(25, 25, 100, 100);
     ctx.clearRect(45, 45, 60, 60);
     ctx.strokeRect(50, 50, 50, 50);
@@ -22,12 +28,8 @@ function drewTriangle() {
   // closePath() - 현재 하위 경로의 시작 부분과 연결된 직선을 추가합니다.
   // stroke() - 윤곽선을 이용하여 도형을 그립니다.
   // fill() - 경로의 내부를 채워서 내부가 채워진 도형을 그립니다.
-  const canvas = document.getElementById(
-    'tutorial'
-  ) as HTMLCanvasElement | null;
-  if (canvas?.getContext) {
-    const ctx = canvas.getContext('2d')!;
-
+  const ctx = getCtx();
+  if (ctx) {
     ctx.beginPath();
     ctx.moveTo(75, 50);
     ctx.lineTo(100, 75);
@@ -36,7 +38,24 @@ function drewTriangle() {
   }
 }
 
+function drewSmile() {
+  // moveTo(x, y) - 팬을 x와 y로 지정된 좌표로 옮깁니다.
+  const ctx = getCtx();
+  if (ctx) {
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+    ctx.stroke();
+  }
+}
+
 window.document.addEventListener('DOMContentLoaded', () => {
   // drawSquare();
-  drewTriangle();
+  // drewTriangle();
+  drewSmile();
 });
